760ed079ed6ae132102c3d0a9f623d5a6eeb2e7e
OleksandrHavrylchyk <aleksandr.havrylchyk@gmail.com>
2020-02-27 03:42:33 -0800

    Added test header

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,18 +5,20 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { HeaderComponent } from './components/Layout/header/header.component'
+import { FooterComponent } from './components/Layout/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
+    HeaderComponent,
+    FooterComponent,
     HomeComponent,
     CounterComponent,
     FetchDataComponent
